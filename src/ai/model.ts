e7ed891797977780a5339addd21b88f20be11053
Yahya Messelmi <yahyaaoo200@gmail.com>
2025-04-16 17:13:16 +0100

    feat(AI): :sparkles: Add and config the AI model

---
+++ src/ai/model.ts
@@ -0,0 +1,17 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { config } from "dotenv";
+// config the .env
+config();
+
+
+
+// The AI Model
+
+// *NOTE - You can use any AI model you prefer for my case I'm using the google generative AI with 'gemini-2.0-flash' model
+
+export const AIModel = new ChatGoogleGenerativeAI({
+  apiKey: process.env.MODEL_API_SECRET_KEY, // secret key
+  model: "gemini-2.0-flash",
+  maxOutputTokens: 2048,
+  temperature: 0,
+});
